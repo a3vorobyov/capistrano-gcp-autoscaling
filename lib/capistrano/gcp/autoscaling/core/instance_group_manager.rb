af01474b0b3c873f1206bb66d0a12f4fd372dc93
Aleksei Vorobyov <a3.vorobyov@itransition.com>
2021-06-23 19:30:10 +0300

    add an ability to use simple instance groups

--- lib/capistrano/gcp/autoscaling/core/instance_group_manager.rb
+++ lib/capistrano/gcp/autoscaling/core/instance_group_manager.rb
@@ -4,29 +4,14 @@ module Capistrano
   module Gcp
     module Autoscaling
       module Core
-        class InstanceGroupManager
-          def initialize(compute_service, options = {})
-            @compute_service = compute_service
-            @options = options
-          end
-
+        class InstanceGroupManager < Capistrano::Gcp::Autoscaling::Core::InstanceGroup
           def instances
-            group_manager.managed_instances.map(&method(:instance_for)).select(&:available?).sort_by(&:created_at)
+            group.managed_instances.map(&method(:instance_for)).select(&:available?).sort_by(&:created_at)
           end
 
           private
 
-          attr_reader :compute_service, :options
-
-          def instance_for(managed_instance)
-            Capistrano::Gcp::Autoscaling::Core::Instance.new(
-              compute_service,
-              managed_instance,
-              gcp_project_id: options.fetch(:gcp_project_id)
-            )
-          end
-
-          def group_manager
+          def group
             compute_service.list_region_instance_group_manager_managed_instances(
               options.fetch(:gcp_project_id),
               options.fetch(:gcp_region),
